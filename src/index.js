864a907a914631ee3c8f40e11f66f799d70864bd
Miqotes <ashleelennab@gmail.com>
2020-10-20 11:50:17 -0700

    fix grid/box to appear

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ getGrid()
 
 function displayGrid(grid) {
     console.log(grid)
-    debugger
+    
 }
 
 function movePlatformLeft() {
@@ -60,3 +60,35 @@ function animate() {
     x += grid.ball.speed
     y += grid.ball.speed
 }
+
+//// creating grid for box
+//grid width and height
+var bw = 900;
+var bh = 800;
+//padding around grid
+var p = 10;
+//size of canvas
+let cw = bw + (p*2) + 1;
+let ch = bh + (p*2) + 1;
+
+let theCanvas = $('<canvas/>').css({width: cw, height: ch}).appendTo('body');
+
+let context = theCanvas.get(0).getContext("2d");
+
+function drawBoard(){
+    for (let x = 0; x <= bw; x += 40) {
+        context.moveTo(0.5 + x + p, p);
+        context.lineTo(0.5 + x + p, bh + p);
+    }
+
+
+    for (let x = 0; x <= bh; x += 40) {
+        context.moveTo(p, 0.5 + x + p);
+        context.lineTo(bw + p, 0.5 + x + p);
+    }
+
+    context.strokeStyle = "black";
+    context.stroke();
+}
+
+drawBoard();
